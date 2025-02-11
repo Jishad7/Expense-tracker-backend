8e4c9c93a2a009b91b89230947836d5b786d2021
Jishad7 <jishadmohamed4@gmail.com>
2025-02-11 16:11:22 +0530

    url updated

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const expenseRoutes = require('./routes/expenses');
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors({
+    origin: 'https://expense-tracker-three-plum.vercel.app' // Allow your Vercel frontend
+}));app.use(bodyParser.json());
 app.use('/expenses',expenseRoutes);
 
 app.listen(5000,()=>{
-    console.log("Server running on port http://localhost:5000/");
+    console.log("Server running on port https://expense-tracker-three-plum.vercel.app");
 });
