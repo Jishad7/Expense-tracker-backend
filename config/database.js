93b5974fd38d4d33310c59648f2166901ec4ad49
Jishad7 <jishadmohamed4@gmail.com>
2025-02-11 12:59:28 +0530

    backend pg server update

--- config/database.js
+++ config/database.js
@@ -1,14 +1,17 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
+  dialect: 'postgres',
+  dialectOptions: {
     ssl: {
-      rejectUnauthorized: false, // Required for Render to work with PostgreSQL
+      require: true, // Ensures SSL is used
+      rejectUnauthorized: false, // Disables certificate validation (use with caution)
     },
-  });
+  },
+});
 
 sequelize.authenticate()
-    .then(()=>console.log('Connected to postgresql!'))
-    .catch((err)=>console.log('Unable to connect to database',err));
+  .then(() => console.log('Connected to PostgreSQL!'))
+  .catch((err) => console.log('Unable to connect to database:', err));
 
-    module.exports = sequelize;
+module.exports = sequelize;
